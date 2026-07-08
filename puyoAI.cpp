24bb12ec39723b190808cd7ea5b50b92742bc1ad
gata272 <t3871.teru@gmail.com>
2026-07-09 07:54:27 +0900

    puyoAI.cpp を更新

--- puyoAI.cpp
+++ puyoAI.cpp
@@ -60,6 +60,15 @@ Move placeHorizontal(int leftCol, int rightCol, char leftTargetAlpha, Puyo p, st
 }
 
 Move getGTRMove(std::string patternKey, int turn, Puyo p, std::map<int, char>& cm, int& outType) {
+    // AAAA型
+    if (patternKey.substr(0, 5) == "AA-AA") {
+        outType = AAAA;
+        if (patternKey == "AA-AA-AA") { if(turn==0) return placeVertical(2,'\0',p,cm); if(turn==1) return placeVertical(4,'\0',p,cm); if(turn==2) return placeVertical(3,'\0',p,cm); }
+        if (patternKey == "AA-AA-AB") { if(turn==0) return placeHorizontal(1,2,'\0',p,cm); if(turn==1) return placeHorizontal(4,5,'\0',p,cm); if(turn==2) return placeVertical(3,'B',p,cm); }
+        if (patternKey == "AA-AA-BB") { if(turn==0) return placeVertical(3,'\0',p,cm); if(turn==1) return placeVertical(3,'\0',p,cm); if(turn==2) return placeHorizontal(1,2,'\0',p,cm); }
+        if (patternKey == "AA-AA-BC") { if(turn==0) return placeVertical(3,'\0',p,cm); if(turn==1) return placeVertical(3,'\0',p,cm); if(turn==2) return placeVertical(3,'B',p,cm); }
+    }
+
     // AAAB型
     if (patternKey.substr(0, 5) == "AA-AB") {
         outType = AAAB;
@@ -70,6 +79,18 @@ Move getGTRMove(std::string patternKey, int turn, Puyo p, std::map<int, char>& c
         if (patternKey == "AA-AB-BC") { if(turn==0) return placeHorizontal(1,2,'\0',p,cm); if(turn==1) return placeVertical(3,'B',p,cm); if(turn==2) return placeVertical(4,'C',p,cm); }
         if (patternKey == "AA-AB-CC") { if(turn==0) return placeHorizontal(1,2,'\0',p,cm); if(turn==1) return placeVertical(3,'B',p,cm); if(turn==2) return placeHorizontal(1,2,'\0',p,cm); }
         if (patternKey == "AA-AB-CD") { if(turn==0) return placeHorizontal(1,2,'\0',p,cm); if(turn==1) return placeVertical(3,'B',p,cm); if(turn==2) return placeVertical(6,'D',p,cm); }
+    }
+
+    // ABAA型
+    if (patternKey.substr(0, 5) == "AB-AA") {
+        outType = ABAA;
+        if (patternKey == "AB-AA-AA") { if(turn==0) return placeVertical(3,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeHorizontal(4,5,'\0',p,cm); }
+        if (patternKey == "AB-AA-AB") { if(turn==0) return placeVertical(3,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeVertical(4,'A',p,cm); }
+        if (patternKey == "AB-AA-AC") { if(turn==0) return placeVertical(3,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeVertical(2,'C',p,cm); }
+        if (patternKey == "AB-AA-BB") { if(turn==0) return placeVertical(2,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeVertical(1,'B',p,cm); }
+        if (patternKey == "AB-AA-BC") { if(turn==0) return placeVertical(3,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeVertical(4,'C',p,cm); }
+        if (patternKey == "AB-AA-CC") { if(turn==0) return placeVertical(3,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeHorizontal(1,2,'\0',p,cm); }
+        if (patternKey == "AB-AA-CD") { if(turn==0) return placeVertical(3,'B',p,cm); if(turn==1) return placeHorizontal(1,2,'\0',p,cm); if(turn==2) return placeVertical(6,'D',p,cm); }
     }
 
     // AABB型
